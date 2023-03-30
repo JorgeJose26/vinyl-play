61185c492011c01df5ec923bb36c070940ef2add
Jorge Casaudoumecq <Jorge@Casaudoumecq.me>
2023-03-30 14:10:41 -0400

    Created two new componets

---
+++ src/components/vinylCards.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function vinylCards(props) {
+    return (
+        <div>
+
+
+        </div>
+    );
+}
+
+export default vinylCards;
---
+++ src/pages/collections.js
@@ -0,0 +1,16 @@
+
+import NavBar from '@/components/layouts/navBar';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+
+function collections(props) {
+    return (
+        <>
+            <NavBar />
+
+
+
+        </>
+    );
+}
+
+export default collections;
